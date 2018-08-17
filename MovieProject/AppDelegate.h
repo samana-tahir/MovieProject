b75c9caacad71132964288d6882ee608de5f4838
mi-app <app@massiveinfinity.com>
2018-08-17 14:02:13 +0800

    Initial Commit

---
+++ MovieProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieProject
+//
+//  Created by Samana Tahir on 17/08/2018.
+//  Copyright © 2018 Samana Tahir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
